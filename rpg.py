e0c23582917f9433b663af64c95587604ac583a0
Jonathan Acosta <jonathanacosta@Jonathans-MacBook-Pro.local>
2022-09-21 14:02:01 -0400

    Added Rpg file to create character code, left all comments on main.py to create story

---
+++ rpg.py
@@ -0,0 +1,21 @@
+
+class Rpg:
+
+    def __init__(self):
+        self.health_bar = 100
+        self.quick_attack = 20
+        self.sword_slash = 40
+        self.mega_kick = 35
+        self.mega_punch = 30
+        self.potion = 30
+
+    def hercules_health(self, health):
+        self.potion += health
+
+# hercules = {"Health Bar": 100, "Attack Stat": 300, "Defense Stat":}
+# hercules_attacks = {"Quick Attack": 15, "Sword Slash": 30, "Mega Kick": 35, }
+
+
+# health_items = {"potion": 15, "super potion": 30, "hyper potion": 55, "full restore": 100}
+# zombie_minion = {"Health Bar": 35}
+# def minion_battle(health, ):
